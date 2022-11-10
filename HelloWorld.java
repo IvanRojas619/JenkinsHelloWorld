32f443810dad41477531cf575d4721fb03e1dbed
IvanRojas619 <61441203+IvanRojas619@users.noreply.github.com>
2022-11-10 14:44:41 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public static void main(String args[]){
 
 System.out.println("Hello World!!!");
   System.out.println("Viva México ... Made some changes");
-  System.out.println(":D:D:D:D:D:D:D:D:D:D");
+  System.out.println("GEGEGEGEGEGEGEGG");
 
 }
 
